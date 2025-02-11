af871045c3fc632a1d8a9faf801a047cc923d48a
kranthi1818 <kranthikumar1018@gmail.com>
2025-02-11 16:58:32 +0530

    made changes to 8 problem

--- server/8-highest-number-one-dismissed-by-another.js
+++ server/8-highest-number-one-dismissed-by-another.js
@@ -7,25 +7,67 @@ let deliveriesData = JSON.parse(fs.readFileSync('./data/deliveries.json', 'utf8'
 
 function highestNumberOneDismissedByAnother(matchesData,deliveriesData){
 
-    let result  = deliveriesData.reduce((acc,curr)=>{
 
-       
-        if(curr['dismissal_kind'] !== 'run out' && curr['player_dismissed']){
+  let result  = deliveriesData.reduce((acc,curr)=>{
 
-            let key = curr.bowler + "-" +curr.player_dismissed
-          if(!acc[key]){
-            acc[key] = 0
-          }
-          acc[key] +=1
+    if(curr.dismissal_kind != "run out" && curr.player_dismissed != ""){
+
+        if(!acc[curr.batsman]){
+            acc[curr.batsman] = {}
         }
 
-        return acc
-    },{})
-   
-    let sorted = Object.fromEntries( Object.entries(result).sort((a,b)=>b[1]-a[1]).slice(0,1))
+       if(! acc[curr.batsman][curr.bowler] ){
+        acc[curr.batsman][curr.bowler]  = 0
+       }
+       acc[curr.batsman][curr.bowler]  += 1
+    }
+    return acc
+},{})
+
+
+
+
+let max  =-1
+let  maxBowler = ''
+let  maxBatsman
+let finalObj = {}
+for(let batsman in result){
+    for(let bowler in result[batsman]){
+        if(result[batsman][bowler] > max){
+            max = result[batsman][bowler]
+            maxBowler = bowler
+            maxBatsman = batsman
+        }
+    }
     
-    // console.log(sorted)
-    return sorted
+}
+finalObj[maxBatsman]  ={[maxBowler]:max}
+
+
+return finalObj
+
+
+    // let result  = deliveriesData.reduce((acc,curr)=>{
+
+       
+    //     if(curr['dismissal_kind'] !== 'run out' && curr['player_dismissed'] !== ""){
+
+    //         let key = curr.bowler + "-" +curr.player_dismissed
+    //       if(!acc[key]){
+    //         acc[key] = 0
+    //       }
+    //       acc[key] +=1
+    //     }
+
+    //     return acc
+    // },{})
+
+   
+    // let sorted = Object.fromEntries( Object.entries(result).sort((a,b)=>b[1]-a[1]))
+    
+    // // console.log(sorted)
+    // return sorted
+
 }
 
 let output =    highestNumberOneDismissedByAnother(matchesData,deliveriesData)
